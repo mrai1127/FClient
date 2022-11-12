ae585f2e249a64ed33aa59deeccc6de8bb018259
Mausam Rai <mausamrai11@gmail.com>
2022-11-11 23:05:15 -0500

    second commit

---
+++ src/main/java/com/rai/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.rai.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class DemoController {
+
+	@GetMapping("/name")
+	public String getName() {
+		return "Playing java";
+	}
+
+	@GetMapping("/address")
+	public String getAddress() {
+		return "Atlanta";
+	}
+
+	@GetMapping("/status")
+	public String getStatus() {
+		return "inactive";
+	}
+}
